69988bfca1b6fc438ee8d852d08209544e89aaac
Sisekelo <sidlamini15@alustudent.com>
2017-12-11 10:48:32 +0200

    exec

--- admin/addexec.php
+++ admin/addexec.php
@@ -8,17 +8,17 @@ include('../store/connect.php');
 	/*if (!isset($_FILES['image']['tmp_name'])) {
 	echo "";
 	}else{*/
-	$file=$_FILES['image']['tmp_name'];
+	/*$file=$_FILES['image']['tmp_name'];
 	$image= addslashes(file_get_contents($_FILES['image']['tmp_name']));
 	$image_name= addslashes($_FILES['image']['name']);
-	$image_size= getimagesize($_FILES['image']['tmp_name']);
+	$image_size= getimagesize($_FILES['image']['tmp_name']);*/
 
 	
-		if ($image_size==FALSE) {
+		/*if ($image_size==FALSE) {
 		
 			echo "That's not an image!";
 			
-		}else{
+		}else{*/
 			
 			move_uploaded_file($_FILES["image"]["tmp_name"],"../store/img/products/" . $_FILES["image"]["name"]);
 
@@ -46,7 +46,7 @@ VALUES
 header("location: products.php");
 			exit();
 		
-			}
+			
 	
 
 
