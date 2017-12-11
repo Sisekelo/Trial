07be3ec7d0096899020e486117914503b84a4610
Sisekelo <sidlamini15@alustudent.com>
2017-12-11 09:52:50 +0200

    Update connect.php

--- store/connect.php
+++ store/connect.php
@@ -1,7 +1,7 @@
 <?php
 
 $host = 'mysql.hostinger.com';
-$user = '	u627368589_trial';
+$user = 'u627368589_trial';
 $pass = '12345678';
 $db = '	u627368589_trial';
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
