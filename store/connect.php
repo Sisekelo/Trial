f9139eae72721202134de63e6f4a832043ca708f
Sisekelo <sidlamini15@alustudent.com>
2017-12-15 11:48:10 +0200

    a

--- store/connect.php
+++ store/connect.php
@@ -1,9 +1,9 @@
 <?php
-
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'shoopingcart';
+/* Database connection settings */
+$host = 'mysql.hostinger.com';
+$user = 'u627368589_trial';
+$pass = '12345678';
+$db = 'u627368589_trial';;
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
 
 
