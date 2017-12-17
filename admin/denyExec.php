2e3320a080df0018cabc1d4facc37b3bf009c23c
Sisekelo <sidlamini15@alustudent.com>
2017-12-17 10:19:34 +0200

    a

--- admin/denyExec.php
+++ admin/denyExec.php
@@ -7,12 +7,19 @@ $id = $_POST['ID'];
 $number = $_POST['Number'];
 $Reason = $_POST['Reason'];
 
- $givepoints =$mysqli->query("UPDATE Orders2 SET Deny='1' WHERE Id='$id'") or die($mysqli->error());
+ $givepoints =$mysqli->query("UPDATE Orders2 SET Deny='1' WHERE Id='$id'") or die($mysqli->error()); //update db saying we denied order
 
+//send SMS to user
 
-/*echo $numberPlus."<br>";
-echo $number;*/
+	include ( "Nexmo-PHP-lib-master/NexmoMessage.php" ); 
+	/*$conmessage = 'Your order has just been confirmed. It is coming soon ;) Home: http://ouideliver.xyz/index.php';*/
+	// Step 1: Declare new NexmoMessage.
+	$nexmo_sms = new NexmoMessage('d6726b9a', '005e2f3453ccb56c');
+	// Step 2: Use sendText( $to, $from, $message ) method to send a message. 
+	$info = $nexmo_sms->sendText( $number, 'MyApp', $Reason);
 
-//check Id
+	/*echo $nexmo_sms->displayOverview($info);*/
+
+	header("location: https://ouideliver.xyz/Trial/admin/index.php?vendor=1.php");
 
 ?>
