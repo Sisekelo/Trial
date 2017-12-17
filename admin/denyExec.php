9da14db97b9e3da4be5c576769faf1af6ea4e4c0
Sisekelo <sidlamini15@alustudent.com>
2017-12-17 11:42:49 +0200

    a

--- admin/denyExec.php
+++ admin/denyExec.php
@@ -8,6 +8,8 @@ $number = $_POST['Number'];
 $Reason = $_POST['Reason'];
 $Vendor = $_POST['Vendor'];
 
+$message = .$Vendor." denied your order because: ".$Reason.;
+
  $givepoints =$mysqli->query("UPDATE Orders2 SET Deny='1' WHERE Id='$id'") or die($mysqli->error()); //update db saying we denied order
 
 //send SMS to user
@@ -17,7 +19,7 @@ $Vendor = $_POST['Vendor'];
 	// Step 1: Declare new NexmoMessage.
 	$nexmo_sms = new NexmoMessage('d6726b9a', '005e2f3453ccb56c');
 	// Step 2: Use sendText( $to, $from, $message ) method to send a message. 
-	$info = $nexmo_sms->sendText( $number, 'MyApp', $Reason);
+	$info = $nexmo_sms->sendText( $number, 'MyApp', $message);
 
 	/*echo $nexmo_sms->displayOverview($info);*/
 
